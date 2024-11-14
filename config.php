dab463aa984b31e769337c46c2e87b2e2a89602f
Lilia Borges <lilia.bsilveira@sp.senac.br>
2024-11-14 14:05:43 -0300

    Correções de Segurança

--- config.php
+++ config.php
@@ -1,12 +1,4 @@
 <?php
-define("URL", "http://localhost/PROJETOS_24/rest/rest.php");
-define("CHAVE_SECRETA", 'S3C43T*$CH@V3');
-
-// Função para codificar em Base64 URL-safe
-function base64UrlEncode($data) {
-    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
-}
-// Função para decodificar Base64 URL-safe
-function base64UrlDecode($data) {
-    return base64_decode(strtr($data, '-_', '+/'));
-}
+define("URL", "http://localhost/api/rest.php");
+define("URLS", "http://localhost/api/servers.php");
+define('CHAVE_SECRETA', 'S3NH@53C43T@');
